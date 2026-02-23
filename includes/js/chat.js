2d1af156d0e293572ee710acd824e6cd3ee925c7
Mustak Ansary <mustak65ee@gmail.com>
2026-02-23 14:00:50 +0530

    sovled the issue regarding chat problem

--- includes/js/chat.js
+++ includes/js/chat.js
@@ -1,20 +1,36 @@
 $(function() {
     "use strict";
 
-    const ps5 = new PerfectScrollbar('#ChatBody', {
-        useBothWheelAxes: true,
-        suppressScrollX: true,
-    });
-    const ps6 = new PerfectScrollbar('.profile-details-main', {
-        useBothWheelAxes: true,
-        suppressScrollX: true,
-    });
-    const ps7 = new PerfectScrollbar('.main-chat-contacts-slider', {
-        useBothWheelAxes: true,
-        suppressScrollY: true,
-    });
-    const ps18 = new PerfectScrollbar('.main-chat-2', {
-        useBothWheelAxes: true,
-        suppressScrollX: true,
-    });
+    // Initialize PerfectScrollbar only if elements exist
+    const chatBody = document.querySelector('#ChatBody');
+    if (chatBody) {
+        const ps5 = new PerfectScrollbar('#ChatBody', {
+            useBothWheelAxes: true,
+            suppressScrollX: true,
+        });
+    }
+
+    const profileDetails = document.querySelector('.profile-details-main');
+    if (profileDetails) {
+        const ps6 = new PerfectScrollbar('.profile-details-main', {
+            useBothWheelAxes: true,
+            suppressScrollX: true,
+        });
+    }
+
+    const contactsSlider = document.querySelector('.main-chat-contacts-slider');
+    if (contactsSlider) {
+        const ps7 = new PerfectScrollbar('.main-chat-contacts-slider', {
+            useBothWheelAxes: true,
+            suppressScrollY: true,
+        });
+    }
+
+    const mainChat2 = document.querySelector('.main-chat-2');
+    if (mainChat2) {
+        const ps18 = new PerfectScrollbar('.main-chat-2', {
+            useBothWheelAxes: true,
+            suppressScrollX: true,
+        });
+    }
 });
