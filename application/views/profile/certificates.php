2efc6e0bb757e4ca7fd105ddbccbdf48ae57a98a
Mustak Ansary <mustak65ee@gmail.com>
2026-03-13 17:55:19 +0530

    updated the Audit Report , fixes the Din Certificate system , updated the Fim system

--- application/views/profile/certificates.php
+++ application/views/profile/certificates.php
@@ -45,35 +45,35 @@
                                     <div class="row mb-4">
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label><strong>Audit Report</strong></label>
+                                                <label><strong>Company Registration Certificate</strong></label>
                                                 <?php if(!empty($kyc) && !empty($kyc['audit_report'])){ ?>
                                                     <div class="mt-2">
                                                         <a href="<?= $kyc['audit_report'] ?>" target="_blank" class="btn btn-sm btn-info">
-                                                            <i class="fa fa-eye"></i> View Audit Report
+                                                            <i class="fa fa-eye"></i> View Company Registration Certificate
                                                         </a>
                                                         <a href="<?= base_url('profile/download_certificate/audit_report') ?>" class="btn btn-sm btn-success">
-                                                            <i class="fa fa-download"></i> Download Audit Report
+                                                            <i class="fa fa-download"></i> Download Company Registration Certificate
                                                         </a>
                                                     </div>
                                                 <?php } else { ?>
-                                                    <p class="text-muted">Audit Report not uploaded yet.</p>
+                                                    <p class="text-muted">Company Registration Certificate not uploaded yet.</p>
                                                 <?php } ?>
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label><strong>Income Tax Certificate</strong></label>
+                                                <label><strong>DIN Certificate</strong></label>
                                                 <?php if(!empty($kyc) && !empty($kyc['income_tax_certificate'])){ ?>
                                                     <div class="mt-2">
                                                         <a href="<?= $kyc['income_tax_certificate'] ?>" target="_blank" class="btn btn-sm btn-info">
-                                                            <i class="fa fa-eye"></i> View Income Tax Certificate
+                                                            <i class="fa fa-eye"></i> View DIN Certificate
                                                         </a>
                                                         <a href="<?= base_url('profile/download_certificate/income_tax_certificate') ?>" class="btn btn-sm btn-success">
-                                                            <i class="fa fa-download"></i> Download Income Tax Certificate
+                                                            <i class="fa fa-download"></i> Download DIN Certificate
                                                         </a>
                                                     </div>
                                                 <?php } else { ?>
-                                                    <p class="text-muted">Income Tax Certificate not uploaded yet.</p>
+                                                    <p class="text-muted">DIN Certificate not uploaded yet.</p>
                                                 <?php } ?>
                                             </div>
                                         </div>
